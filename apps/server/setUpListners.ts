5f123fbddf6fcbb824fa4bf4fe43839529ad77e3
avayyyyyyy <jabhi465@gmail.com>
2024-03-07 16:30:37 +0530

    ✨feature: Initialized setUpListner file

--- apps/server/setUpListners.ts
+++ apps/server/setUpListners.ts
@@ -1,5 +1,7 @@
-import { Socket } from "dgram";
 import { Server } from "socket.io";
+import { Game } from "./Game";
+
+export const room = new Map<string, Game>();
 
 export function setUpListners(io: Server) {
   io.on("connection", (socket) => {
@@ -9,6 +11,17 @@ export function setUpListners(io: Server) {
       if (!name) return socket.emit("error", "Name not valid");
 
       socket.join(roomId);
+
+      if (room.has(roomId)) {
+        const game = room.get(roomId);
+
+        if (!game) return socket.emit("error", "Game not found");
+        game.joinGame(socket.id, name, socket);
+      } else {
+        const game = new Game(roomId, io, socket.id);
+        room.set(roomId, game);
+        game.joinGame(socket.id, name, socket);
+      }
     });
   });
 }
