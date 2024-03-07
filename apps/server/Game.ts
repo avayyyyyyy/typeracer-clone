9aa6a5e65faf646735258287ab5b04dfd1fe706c
avayyyyyyy <jabhi465@gmail.com>
2024-03-07 16:28:33 +0530

    ✨feature: Implemented feature of leaving and disconnecting

--- apps/server/Game.ts
+++ apps/server/Game.ts
@@ -1,5 +1,6 @@
-import { setUpListners } from "./setUpListners";
-import { Server, Socket, Socket } from "socket.io";
+import { room, setUpListners } from "./setUpListners";
+import { Server, Socket } from "socket.io";
+import { generateParagraph } from "./utils/para";
 
 export class Game {
   gameStatus: "not-started" | "in-progress" | "completed";
@@ -7,7 +8,7 @@ export class Game {
   players: { name: string; id: string; score: number }[];
   io: Server;
   gameHost: string;
-  paragraph: "";
+  paragraph: string;
 
   constructor(id: string, io: Server, host: string) {
     this.gameID = id;
@@ -18,7 +19,90 @@ export class Game {
     this.io = io;
   }
 
-  setUpListners(socket: Socket) {}
+  setUpListners(socket: Socket) {
+    socket.on("start-game", async () => {
+      if (this.gameHost !== socket.id) {
+        return socket.emit("error", "you are not the host! ");
+      }
+      if (this.gameStatus === "in-progress") {
+        return socket.emit("error", "Game is already started");
+      }
+
+      for (const player of this.players) {
+        player.score = 0;
+      }
+
+      this.io.to(this.gameID).emit("player", this.players);
+
+      this.gameStatus = "in-progress";
+
+      const paragraph = await generateParagraph();
+      this.paragraph = paragraph;
+      this.io.to(this.gameID).emit("game-started", paragraph);
+
+      setTimeout(() => {
+        this.gameStatus = "completed";
+        this.io.to(this.gameID).emit("game-finished");
+        this.io.to(this.gameID).emit("player", this.players);
+      }, 60000);
+
+      socket.on("player-typed", (typed: string) => {
+        if (this.gameStatus !== "in-progress")
+          return socket.emit("error", "The game has not started yet");
+
+        const splittedParagraph = this.paragraph.split(" ");
+        const splittedTyped = typed.split(" ");
+
+        let score = 0;
+
+        for (let i = 0; i < splittedTyped.length; i++) {
+          if (splittedTyped[i] === splittedParagraph[i]) {
+            score++;
+          } else {
+            break;
+          }
+        }
+
+        const player = this.players.find((player) => player.id === socket.id);
+
+        if (player) {
+          player.score = score;
+        }
+
+        this.io.to(this.gameID).emit("players", { id: socket.id, score });
+      });
+
+      socket.on("leave", () => {
+        if (socket.id === this.gameHost) {
+          this.players = this.players.filter(
+            (player) => player.id !== this.gameHost
+          );
+
+          this.gameHost = this.players[0].id;
+        } else {
+          room.delete(this.gameID);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        if (socket.id === this.gameHost) {
+          this.players = this.players.filter(
+            (player) => player.id !== socket.id
+          );
+
+          if (this.players.length !== 0) {
+            this.gameHost = this.players[0].id;
+          } else {
+            // Delete the game if the host leaves and there are no players
+            room.delete(this.gameID);
+          }
+        }
+
+        socket.leave(this.gameID);
+        this.players = this.players.filter((player) => player.id !== socket.id);
+      });
+    });
+  }
 
   joinGame(id: string, name: string, socket: Socket) {
     if (this.gameStatus === "in-progress") {
